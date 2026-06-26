e886739b3eb09b2b766854c835ed0c4f03aac9bf
fengzhilaoling <fengzhilaoling@163.com>
2026-06-26 16:59:42 +0800

    fix: 完善用户管理相关功能

--- zabbix-maint/internal/adapter/v5/user.go
+++ zabbix-maint/internal/adapter/v5/user.go
@@ -7,44 +7,130 @@ import (
 	"zabbix-maint/pkg/zabbix"
 )
 
-// UserCreate 创建用户 (V5 版本)
+// UserCreate 创建用户 (V5)
 func (a *V5Adapter) UserCreate(ctx context.Context, req zabbix.UserCreateReq) (string, error) {
-	// TODO: implement V5 user.create
-	return "", fmt.Errorf("not implemented")
+	usrgrps := make([]map[string]string, len(req.UserGroupIDs))
+	for i, gid := range req.UserGroupIDs {
+		usrgrps[i] = map[string]string{"usrgrpid": gid}
+	}
+	params := map[string]interface{}{
+		"alias":   req.Alias,
+		"name":    req.Name,
+		"surname": req.Surname,
+		"passwd":  req.Passwd,
+		"usrgrps": usrgrps,
+		"type":    "1",
+	}
+	var result struct {
+		UserIDs []string `json:"userids"`
+	}
+	if err := a.client.Call(ctx, "user.create", params, &result); err != nil {
+		return "", err
+	}
+	if len(result.UserIDs) == 0 {
+		return "", fmt.Errorf("user.create returned empty")
+	}
+	return result.UserIDs[0], nil
 }
 
 // UserDisable 禁用用户 (V5: status=1)
 func (a *V5Adapter) UserDisable(ctx context.Context, userID string) error {
-	// TODO: implement V5 user.update status=1
-	return fmt.Errorf("not implemented")
+	params := map[string]interface{}{
+		"userid": userID,
+		"status": "1",
+	}
+	return a.client.Call(ctx, "user.update", params, nil)
 }
 
 // UserEnable 启用用户 (V5: status=0)
 func (a *V5Adapter) UserEnable(ctx context.Context, userID string) error {
-	// TODO: implement V5 user.update status=0
-	return fmt.Errorf("not implemented")
+	params := map[string]interface{}{
+		"userid": userID,
+		"status": "0",
+	}
+	return a.client.Call(ctx, "user.update", params, nil)
 }
 
 // UserUpdatePassword 修改密码
 func (a *V5Adapter) UserUpdatePassword(ctx context.Context, userID, newPass string) error {
-	// TODO: implement V5 user.update passwd
-	return fmt.Errorf("not implemented")
+	params := map[string]interface{}{
+		"userid": userID,
+		"passwd": newPass,
+	}
+	return a.client.Call(ctx, "user.update", params, nil)
 }
 
 // UserDelete 删除用户
 func (a *V5Adapter) UserDelete(ctx context.Context, userID string) error {
-	// TODO: implement V5 user.delete
-	return fmt.Errorf("not implemented")
+	return a.client.Call(ctx, "user.delete", []string{userID}, nil)
 }
 
 // UserList 查询用户列表
 func (a *V5Adapter) UserList(ctx context.Context, filter string) ([]zabbix.UnifiedUser, error) {
-	// TODO: implement V5 user.get
-	return nil, fmt.Errorf("not implemented")
+	params := map[string]interface{}{
+		"output":        "extend",
+		"selectUsrgrps": "extend",
+	}
+	if filter != "" {
+		params["search"] = map[string]string{"alias": filter}
+	}
+	var users []rawUserV5
+	if err := a.client.Call(ctx, "user.get", params, &users); err != nil {
+		return nil, err
+	}
+	result := make([]zabbix.UnifiedUser, len(users))
+	for i, u := range users {
+		result[i] = u.toUnified()
+	}
+	return result, nil
 }
 
 // UserDetail 查询用户详情
 func (a *V5Adapter) UserDetail(ctx context.Context, userID string) (*zabbix.UnifiedUser, error) {
-	// TODO: implement V5 user.get
-	return nil, fmt.Errorf("not implemented")
+	params := map[string]interface{}{
+		"userids":       userID,
+		"output":        "extend",
+		"selectUsrgrps": "extend",
+	}
+	var users []rawUserV5
+	if err := a.client.Call(ctx, "user.get", params, &users); err != nil {
+		return nil, err
+	}
+	if len(users) == 0 {
+		return nil, fmt.Errorf("user not found: %s", userID)
+	}
+	u := users[0].toUnified()
+	return &u, nil
+}
+
+// rawUserV5 Zabbix 5.x 原始用户返回
+type rawUserV5 struct {
+	UserID  string `json:"userid"`
+	Alias   string `json:"alias"`
+	Name    string `json:"name"`
+	Surname string `json:"surname"`
+	Status  string `json:"status"`
+	Usrgrps []struct {
+		Usrgrpid string `json:"usrgrpid"`
+		Name     string `json:"name"`
+	} `json:"usrgrps"`
+}
+
+func (u rawUserV5) toUnified() zabbix.UnifiedUser {
+	enabled := u.Status == "0"
+	groupIDs := make([]string, len(u.Usrgrps))
+	groupNames := make([]string, len(u.Usrgrps))
+	for i, g := range u.Usrgrps {
+		groupIDs[i] = g.Usrgrpid
+		groupNames[i] = g.Name
+	}
+	return zabbix.UnifiedUser{
+		ID:         u.UserID,
+		Alias:      u.Alias,
+		Name:       u.Name,
+		Surname:    u.Surname,
+		Enabled:    enabled,
+		GroupIDs:   groupIDs,
+		GroupNames: groupNames,
+	}
 }
--- zabbix-maint/internal/adapter/v7/user.go
+++ zabbix-maint/internal/adapter/v7/user.go
@@ -7,44 +7,139 @@ import (
 	"zabbix-maint/pkg/zabbix"
 )
 
-// UserCreate 创建用户 (V7 版本，需要 roleid)
+// UserCreate 创建用户 (V7)
 func (a *V7Adapter) UserCreate(ctx context.Context, req zabbix.UserCreateReq) (string, error) {
-	// TODO: implement V7 user.create with roleid
-	return "", fmt.Errorf("not implemented")
+	usrgrps := make([]map[string]string, len(req.UserGroupIDs))
+	for i, gid := range req.UserGroupIDs {
+		usrgrps[i] = map[string]string{"usrgrpid": gid}
+	}
+	params := map[string]interface{}{
+		"username": req.Alias,
+		"name":     req.Name,
+		"surname":  req.Surname,
+		"passwd":   req.Passwd,
+		"roleid":   req.RoleID,
+		"usrgrps":  usrgrps,
+	}
+	var result struct {
+		UserIDs []string `json:"userids"`
+	}
+	if err := a.client.Call(ctx, "user.create", params, &result); err != nil {
+		return "", err
+	}
+	if len(result.UserIDs) == 0 {
+		return "", fmt.Errorf("user.create returned empty")
+	}
+	return result.UserIDs[0], nil
 }
 
-// UserDisable 禁用用户 (V7: roleid=<disabled_role_id>)
+// UserDisable 禁用用户 (V7: 设为 Guest role=1)
 func (a *V7Adapter) UserDisable(ctx context.Context, userID string) error {
-	// TODO: implement V7 user.update with disabled role
-	return fmt.Errorf("not implemented")
+	params := map[string]interface{}{
+		"userid": userID,
+		"roleid": "1",
+	}
+	return a.client.Call(ctx, "user.update", params, nil)
 }
 
-// UserEnable 启用用户 (V7: roleid=<default_user_role_id>)
+// UserEnable 启用用户 (V7: 恢复为 User role=2)
 func (a *V7Adapter) UserEnable(ctx context.Context, userID string) error {
-	// TODO: implement V7 user.update with default role
-	return fmt.Errorf("not implemented")
+	params := map[string]interface{}{
+		"userid": userID,
+		"roleid": "2",
+	}
+	return a.client.Call(ctx, "user.update", params, nil)
 }
 
 // UserUpdatePassword 修改密码
 func (a *V7Adapter) UserUpdatePassword(ctx context.Context, userID, newPass string) error {
-	// TODO: implement V7 user.update passwd
-	return fmt.Errorf("not implemented")
+	params := map[string]interface{}{
+		"userid": userID,
+		"passwd": newPass,
+	}
+	return a.client.Call(ctx, "user.update", params, nil)
 }
 
 // UserDelete 删除用户
 func (a *V7Adapter) UserDelete(ctx context.Context, userID string) error {
-	// TODO: implement V7 user.delete
-	return fmt.Errorf("not implemented")
+	return a.client.Call(ctx, "user.delete", []string{userID}, nil)
 }
 
 // UserList 查询用户列表
 func (a *V7Adapter) UserList(ctx context.Context, filter string) ([]zabbix.UnifiedUser, error) {
-	// TODO: implement V7 user.get
-	return nil, fmt.Errorf("not implemented")
+	params := map[string]interface{}{
+		"output":        "extend",
+		"selectUsrgrps": "extend",
+		"selectRole":    "extend",
+	}
+	if filter != "" {
+		params["search"] = map[string]string{"username": filter}
+	}
+	var users []rawUserV7
+	if err := a.client.Call(ctx, "user.get", params, &users); err != nil {
+		return nil, err
+	}
+	result := make([]zabbix.UnifiedUser, len(users))
+	for i, u := range users {
+		result[i] = u.toUnified()
+	}
+	return result, nil
 }
 
 // UserDetail 查询用户详情
 func (a *V7Adapter) UserDetail(ctx context.Context, userID string) (*zabbix.UnifiedUser, error) {
-	// TODO: implement V7 user.get
-	return nil, fmt.Errorf("not implemented")
+	params := map[string]interface{}{
+		"userids":       userID,
+		"output":        "extend",
+		"selectUsrgrps": "extend",
+		"selectRole":    "extend",
+	}
+	var users []rawUserV7
+	if err := a.client.Call(ctx, "user.get", params, &users); err != nil {
+		return nil, err
+	}
+	if len(users) == 0 {
+		return nil, fmt.Errorf("user not found: %s", userID)
+	}
+	u := users[0].toUnified()
+	return &u, nil
+}
+
+// rawUserV7 Zabbix 7.x 原始用户返回
+type rawUserV7 struct {
+	UserID  string `json:"userid"`
+	Username string `json:"username"`
+	Name    string `json:"name"`
+	Surname string `json:"surname"`
+	RoleID  string `json:"roleid"`
+	Role    struct {
+		RoleID string `json:"roleid"`
+		Name   string `json:"name"`
+	} `json:"role"`
+	Usrgrps []struct {
+		Usrgrpid string `json:"usrgrpid"`
+		Name     string `json:"name"`
+	} `json:"usrgrps"`
+}
+
+func (u rawUserV7) toUnified() zabbix.UnifiedUser {
+	groupIDs := make([]string, len(u.Usrgrps))
+	groupNames := make([]string, len(u.Usrgrps))
+	for i, g := range u.Usrgrps {
+		groupIDs[i] = g.Usrgrpid
+		groupNames[i] = g.Name
+	}
+	// V7 中 roleid=1 通常对应 Guest（受限），视为禁用
+	enabled := u.RoleID != "1"
+	return zabbix.UnifiedUser{
+		ID:         u.UserID,
+		Alias:      u.Username,
+		Name:       u.Name,
+		Surname:    u.Surname,
+		Enabled:    enabled,
+		RoleID:     u.RoleID,
+		RoleName:   u.Role.Name,
+		GroupIDs:   groupIDs,
+		GroupNames: groupNames,
+	}
 }
